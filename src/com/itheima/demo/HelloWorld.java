b731aff360977d404512719fbc32823cdd305937
Juan-ysj <13886217949@163.com>
2019-08-06 18:52:40 +0800

    这是第一次在eclipse中提交到git服务器的代码

---
+++ src/com/itheima/demo/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.itheima.demo;
+
+import java.util.List;
+
+public class HelloWorld {
+	
+	
+	//alt+/  main
+	public static void main(String[] args) {
+		System.out.println("hello hhh");
+		
+		String s ="余姝娟";
+		System.out.println(s);
+		
+	}
+
+}
